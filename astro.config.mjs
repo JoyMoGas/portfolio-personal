667878cc7f73a7e41e309de5babd66d50e8e0ac8
JoyMoGas <joseantoniomontanogastelum5@gmail.com>
2026-02-21 19:36:30 -0700

    Add Netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,23 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+
   vite: {
     plugins: [tailwindcss()]
   },
+
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'es',],
     routing: {
       prefixDefaultLocale: true
     }
-  }
+  },
+
+  adapter: netlify(),
 });
